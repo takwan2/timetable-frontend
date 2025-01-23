5bdaabed97c64156a4c90493bbe972ff145d74e1
takwan <takwan01@outlook.jp>
2025-01-23 21:15:38 +0900

    change endpoint

--- src/app/constants.js
+++ src/app/constants.js
@@ -1,2 +1,2 @@
-export const ENDPOINT = "http://localhost:8080";
-// export const ENDPOINT = "https://zcwxhpirth.ap-northeast-1.awsapprunner.com";
+// export const ENDPOINT = "http://localhost:8080";
+export const ENDPOINT = "https://nmjqt3ynvq.ap-northeast-1.awsapprunner.com";
